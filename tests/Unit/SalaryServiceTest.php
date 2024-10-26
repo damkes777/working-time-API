ac20b365394a460cfc207ae006088b6a1886a0e7
Damian Kędra <damiankedra2@gmail.com>
2024-10-26 14:13:25 +0200

    add more test case to salary service test

--- tests/Unit/SalaryServiceTest.php
+++ tests/Unit/SalaryServiceTest.php
@@ -7,6 +7,42 @@ use Tests\TestCase;
 
 class SalaryServiceTest extends TestCase
 {
+    public function test_calculate_daily_overtime(): void
+    {
+        $salaryService      = new SalaryService();
+        $hours = 8;
+        $overTime = $salaryService->calculateOvertime($hours, true);
+
+        $this->assertEquals(0, $overTime);
+    }
+
+    public function test_calculate_daily_overtime_with_overtime(): void
+    {
+        $salaryService      = new SalaryService();
+        $hours = 10;
+        $overTime = $salaryService->calculateOvertime($hours, true);
+
+        $this->assertEquals(2, $overTime);
+    }
+
+    public function test_calculate_monthly_overtime(): void
+    {
+        $salaryService      = new SalaryService();
+        $hours = 40;
+        $overTime = $salaryService->calculateOvertime($hours);
+
+        $this->assertEquals(0, $overTime);
+    }
+
+    public function test_calculate_monthly_overtime_with_overtime(): void
+    {
+        $salaryService      = new SalaryService();
+        $hours = 50;
+        $overTime = $salaryService->calculateOvertime($hours);
+
+        $this->assertEquals(10, $overTime);
+    }
+
     public function test_calculate_daily_salary(): void
     {
         $salaryService      = new SalaryService();
